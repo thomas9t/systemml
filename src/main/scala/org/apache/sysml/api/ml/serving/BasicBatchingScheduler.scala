d01c0c9673c33d2e7e381f4730185f7213cfd013
Anthony Thomas <ahthomas@eng.ucsd.edu>
2019-04-16 17:36:11 -0700

    Updates to serving code

--- src/main/scala/org/apache/sysml/api/ml/serving/BasicBatchingScheduler.scala
+++ src/main/scala/org/apache/sysml/api/ml/serving/BasicBatchingScheduler.scala
@@ -48,6 +48,7 @@ object BasicBatchingScheduler extends BatchingScheduler {
 
         val nextBatchSize = min(modelQueues.get(nextModel).size(),
             getOptimalBatchSize(nextModel, execType))
+        System.err.println(s"Next Model: ${nextModel} => Batch Size: ${nextBatchSize}")
         (nextModel, nextBatchSize)
     }
 
